eab414562debaca2d448adc4e6311cafe8ba6d0a
stefanusvemas <stefanusvemas@gmail.com>
2023-11-19 22:00:29 +0700

    Add modal for confirmation dialog in done.blade.php

--- resources/views/admin/done.blade.php
+++ resources/views/admin/done.blade.php
@@ -25,7 +25,7 @@
                                         <td>PASAR PRAMUKA LT DASAR AKS 159 PS.PRAMUKA</td>
                                         <td>TRIR002</td>
                                         <td>167</td>
-                                        <td><a href="#">Konfirmasi</a></td>
+                                        <td><a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal">Konfirmasi</a></td>
                                     </tr>
                                     <tr>
                                         <th scope="row">2</th>
@@ -48,7 +48,23 @@
                 </div>
 
             </div>
-
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Konfirmasi</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            Apakah pengiriman telah selesai?
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                            <a href="/admin" class="btn btn-primary">Selesai</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     </div>
